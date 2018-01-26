3c757e0657abcc7804832b4d318375f988df0cfe
Jacob <jerling1989@gmail.com>
2018-01-26 13:36:26 -0500

    JS comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-
-
+// VARIABLES FOR THE GAME
 var totalWins = 0;
 var totalLosses = 0;
 var guessesLeft;
@@ -9,114 +8,119 @@ var lettersUsed;
 var actor;
 var userInput;
 
-  function startGame(){
-    
 
-    //Displays Total User Wins
-    document.getElementById("totalWins").innerHTML=("<p>WINS: </p>" + totalWins);
+// FUCNTION FOR GAME TO RUN AND 
+// PICK AN ACTOR FOR THE HANGMAN ROUND
+function startGame(){
+  
 
-    //Displays Total User Losses
-    document.getElementById("totalLosses").innerHTML=("<p>LOSSES: </p>" + totalLosses)
+  // DISPLAYS TOTAL USER WINS
+  document.getElementById("totalWins").innerHTML = ("<p>WINS: </p>" + totalWins);
 
-    //Total Number of Guesses User Has Left
-    guessesLeft=13;
-    document.querySelector("#guessesLeft").innerHTML=("<p> GUESSES LEFT: </p>" + guessesLeft);
+  // DISPLAYS TOTAL USER LOSSES
+  document.getElementById("totalLosses").innerHTML = ("<p>LOSSES: </p>" + totalLosses)
 
-
-    //Actors That Have Played Cowboys!
-    actorArray=["JOHN WAYNE", "CLINT EASTWOOD", "KURT RUSSELL", "RUSSELL CROWE", "JEFF BRIDGES"];
-
-    //Turns brackets Into An Array
-    brackets=[];
-
-    //Letters That the User Has Guessed
-    lettersUsed=[];
-    document.querySelector("#lettersUsed").innerHTML=("<p>LETTERS USED: </p>" + lettersUsed);    
-
-    //Picks a Random Actor From the actorArray
-    actorIndex= Math.floor(Math.random() * actorArray.length);
-    actor= actorArray[actorIndex];
-    console.log("New actor :" + actor);
+  // THE TOTAL NUMBER OF GUESSES THE USER HAS LEFT
+  guessesLeft = 13;
+  document.querySelector("#guessesLeft").innerHTML = ("<p> GUESSES LEFT: </p>" + guessesLeft);
 
 
-    for(var i=0; i<actor.length; i++){
-      //brackets array
-      brackets[i]= "_ ";
-    }
-    console.log("Blank Word: " + brackets);
+  // ARRAY OF ACTORS THAT HAVE PLAYED COWBOYS
+  actorArray = ["JOHN WAYNE", "CLINT EASTWOOD", "KURT RUSSELL", "RUSSELL CROWE", "JEFF BRIDGES"];
 
-    document.getElementById('brackets').innerHTML = brackets.join("");
+  // TURNS BRACKETS INTO AN ARRAY
+  brackets = [];
 
+  // LETTERS THAT THE USER HAS GUESSED
+  lettersUsed = [];
+  document.querySelector("#lettersUsed").innerHTML = ("<p>LETTERS USED: </p>" + lettersUsed);    
 
+  // PICKS A RANDOM ACTOR FROM THE ACTOR ARRAY
+  actorIndex = Math.floor(Math.random() * actorArray.length);
+  actor = actorArray[actorIndex];
+  console.log("New actor :" + actor);
 
-  }//end startGame function
+  // FILLS BRACKETS ARRAY WITH EMPTY SLOT
+  // FOR EVERY LETTER IN ACTOR'S NAME
+  for(var i=0; i<actor.length; i++){
 
+    brackets[i] = "_ ";
+  }
 
+  console.log("Blank Word: " + brackets);
+
+  document.getElementById('brackets').innerHTML = brackets.join("");
+
+}// END START GAME FUNCTION
+
+// CALL FUNCTION AT PAGE LOAD
 startGame();  
 
+// FUNCTION TO START GAME WHEN SPACEBAR IS HIT
 document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-        //your code
+  if(e.keyCode == 32){
 
+    // FUNCTION RUN EVERYTIME USER HITS A LETTER
+    document.onkeyup = function(event){
 
+      // TURNS USER LETTER INTO CAPITAL LETTER AND RUNS CHECKLETTER FUNCTION
+      userInput = event.key.toUpperCase();
+      checkLetter(actor, brackets, userInput, lettersUsed);
 
-
-  document.onkeyup=function(event){
-
-    userInput = event.key.toUpperCase();
-    checkLetter(actor, brackets, userInput, lettersUsed);
-
-    console.log(guessesLeft);
-
-    if(guessesLeft==0){
-      console.log("You loose the game!")
-      
-      totalLosses++
-      console.log("Wins: " + totalWins);
       console.log(guessesLeft);
 
-      startGame();
+      // TRACKS A LOSS FOR USER AND RESTARTS GAME WHEN NO GUESSES ARE LEFT
+      if(guessesLeft == 0){
+        console.log("You loose the game!")
+        
+        totalLosses++
+        console.log("Wins: " + totalWins);
+        console.log(guessesLeft);
+
+        startGame();
+      }
+
+      // TRACKS A WIN FOR USER AND RESTARTS GAME WHEN USER GUESSES ACTOR NAME CORRECTLY
+      if(brackets.join("") == actor){
+        console.log("You win the game");
+        
+        totalWins++;
+        console.log("Wins: " + totalWins);
+        console.log(guessesLeft);
+
+        startGame();
+      }
+
     }
-
-
-    if(brackets.join("")== actor){
-      console.log("You win the game");
-      
-      totalWins++;
-      console.log("Wins: " + totalWins);
-      console.log(guessesLeft);
-
-      startGame();
-    }
-
-  }
 
   }
 }
 
 
+// CHECKS IF THE LETTER THE USER GUESSED MATCHES A LETTER IN THE ARRAY
+function checkLetter(actor, brackets, letter, lettersUsed){
 
-  //Checks if the Letter the User Guessed Matches a Letter in the Array
-  function checkLetter(actor, brackets, letter, lettersUsed){
-    for(var i=0; i<actor.length; i++){
-      if(actor.charAt(i)==letter.toUpperCase()){
-        brackets[i]=letter.toUpperCase();
-        console.log(brackets);
-      }
-      document.getElementById("brackets").innerHTML=brackets.join("");
+  // ADDS LETTER TO EMPTY BRACKET IF IT APPEARS IN ACTOR NAME
+  for(var i=0; i<actor.length; i++){
+    if(actor.charAt(i)==letter.toUpperCase()){
+      brackets[i]=letter.toUpperCase();
+      console.log(brackets);
     }
+    document.getElementById("brackets").innerHTML=brackets.join("");
+  }
 
-    if(lettersUsed.indexOf(letter)===-1){
-      guessesLeft--;
-      lettersUsed.push(letter);
+  // ADDS LETTER TO USED LETTERS LIST AND REMOVES GUESS COUNT
+  if(lettersUsed.indexOf(letter)===-1){
+    guessesLeft--;
+    lettersUsed.push(letter);
 
-      for(var i=0; i<lettersUsed.length; i++){
-        document.querySelector("#lettersUsed").innerHTML=("<p>LETTERS USED: </p>" + lettersUsed);  
-      }
+    for(var i=0; i<lettersUsed.length; i++){
+      document.querySelector("#lettersUsed").innerHTML=("<p>LETTERS USED: </p>" + lettersUsed);  
     }
-    
-    document.querySelector("#guessesLeft").innerHTML=("<p> GUESSES LEFT: </p>" + guessesLeft);
-    console.log(lettersUsed);
+  }
+  
+  document.querySelector("#guessesLeft").innerHTML=("<p> GUESSES LEFT: </p>" + guessesLeft);
+  console.log(lettersUsed);
 
 }
 
